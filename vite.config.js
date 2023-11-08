6fb2632aa0a7d4da8fa883ce7ed7dd8ee3fbcbe8
Mikhail Labzin <Mixalich.l@yandex.ru>
2023-11-08 15:27:18 +0300

    Index.html was moved to src folder; Vite config was changed; Server.js for Express was added.

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,32 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
-    plugins: [
-      handlebars({
-        partialDirectory: [resolve(__dirname, 'src/components'), resolve(__dirname, 'src/layouts')],
-      }),
-    ],
-  };
+  root: resolve(__dirname, 'src'),
+
+  plugins: [
+    handlebars({
+      partialDirectory: [resolve(__dirname, 'src/components'), resolve(__dirname, 'src/layouts')],
+    })
+  ],
+
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        "index": resolve(__dirname, 'src/index.html'),
+        "login": resolve(__dirname, 'src/pages/login/login.html'),
+        "create-account": resolve(__dirname, 'src/pages/create-account/create-account.html'),
+        "profile": resolve(__dirname, 'src/pages/profile/profile.html')
+      }
+    }
+  },
+
+  server: {
+    port: 3000
+  },
+
+  preview: {
+    port: 3000
+  }
+};
